a2604c20b4781afd0d14475e786acf526eec83a6
Brett Strydom <brett.strydom@johnlewis.co.uk>
2026-04-08 13:14:41 +0200

    remove the & from start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ export AUDIO_DEVICE_ID=8
 
 export AUDIO_SOUNDS_PATH="/home/brett/projects/node-audio/sound-icons"
 
-node src/app.js >/tmp/node-audio.log 2>&1 &
+node src/app.js >/tmp/node-audio.log 2>&1
